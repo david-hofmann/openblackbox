8ac57e4311d01791790d23e24037b4b9dbddb488
david@nld.ds.mpg.de <david@nld.ds.mpg.de>
2017-10-20 19:25:30 -0400

    Added many new sripts for IRNN and FATT work

--- mlp.py
+++ mlp.py
@@ -8,9 +8,10 @@ import numpy as np
 
 import keras
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.utils import np_utils
+from keras import backend as K
 from time import time
 import matplotlib.pyplot as plt
 
@@ -27,8 +28,10 @@ class AnalyzeWeights(keras.callbacks.Callback):
                       '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
                       '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
 
-    def __init__(self, layers, inputdim, niterations):
+    def __init__(self, layers, inputdim, niterations, act, data):
         self.nlayers = len(layers)
+        self.data = data
+        self.activations = act
         self.w = []
         self.iter = 0
         self.nlinksperunit = [None]*7
@@ -44,28 +47,52 @@ class AnalyzeWeights(keras.callbacks.Callback):
     def on_batch_end(self, batch, logs={}):
         for layer in range(self.nlayers):
             self.w[layer][self.iter, :, :] = np.asarray([self.model.get_weights()[layer * 2], ])
+            if self.iter % 100 == 0:
+
+
         self.iter += 1
 
 
-# def MI_XT(X, T):
-#   # X: input matrix
-#   # T: layer output matrix (activations)
-#   # count words of length N-units, how often do they occur...
-#   words = set(T)
-#   nwords = X.shape
-#   return 12 - len(X)^-1 * np.log2(nwords)
+def coocurrences(data):
+    dt = np.dtype((np.void, data.dtype.itemsize * data.shape[1]))
+    tmp = np.ascontiguousarray(data).view(dt)
+    unq, cnt = np.unique(tmp, return_counts=True)
+    unq = unq.view(data.dtype).reshape(-1, data.shape[1])
+    return dict(zip(tuple(map(tuple, unq)), cnt))
 
 
-batch_size = 256
+def MI_layerwise(X, act):
+    # X: label vector
+    # act: layer output matrix (activations)
+    # count words of length N-units, how often do they occur...
+    N = len(X)
+    MI_XT = 12
+    NT  = coocurrences(act)
+    counts = np.asarray(list(NT.values()))
+    idx = counts > 1  # log(1) = 0 so remove them, they are many and make for loop slow
+    for nT in counts[idx]:
+        MI_XT += -N**-1 * nT * np.log2(nT)
+
+    MI_TY = 1
+    tmp = act[X.astype(bool), :]
+    for (T, nT) in coocurrences(tmp).items():
+        MI_TY += N**-1 * nT * np.log2(nT/NT[T])
+    for (T, nT) in coocurrences(act[np.invert(X.astype(bool)), :]).items():
+        MI_TY += N**-1 * nT * np.log2(nT/NT[T])
+
+    return (MI_XT, MI_TY)
+
+
+
+batch_size = 128
 nb_classes = 2
-nb_epoch = 10000
+nb_epoch = 1000
 saveplots = True
 trainingbatch = 0.85
 
 # the data, shuffled and split between train and test sets
 data = np.loadtxt('data9.dat', dtype='int')
 N = data.shape[0]
-print(data.shape)
 idx = np.arange(0, data.shape[0])
 np.random.shuffle(idx)
 data = data[idx, :]
@@ -87,11 +114,11 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 unitsperlayer = (12, 10, 7, 5, 4, 3, 2)
 nlayers = len(unitsperlayer)
 (ntrainsamples, inputdim) = X_train.shape
-niterations = round(ntrainsamples/batch_size)*nb_epoch
+niterations = int(np.ceil(ntrainsamples/batch_size)*nb_epoch)
 
 model = Sequential()
 for i, n in enumerate(unitsperlayer):
-    if i == 0:    # first layer
+    if i == 0:              # first layer
         model.add(Dense(n, input_shape=(inputdim,)))
         model.add(Activation('tanh'))
     elif i == nlayers-1:    # last layer
@@ -110,9 +137,23 @@ analyzeweights = AnalyzeWeights(unitsperlayer, inputdim, niterations)
 
 history = model.fit(X_train, Y_train,
                     batch_size=batch_size, epochs=nb_epoch,
-                    verbose=1, callbacks=[analyzeweights])
+                    verbose=0, callbacks=[analyzeweights])
 
-print(analyzeweights.iter)
+t1 = time()
+# compute information plane
+inp = model.input                                           # input placeholder
+outputs = [layer.output for layer in model.layers if layer.name[:layer.name.index('_')] == 'activation']  # all layer outputs
+functor = K.function([inp], outputs )                       # evaluation function
+
+activations = functor([data[:, :-1]])
+activations = [np.digitize(a, np.linspace(-1, 1, 30)) for a in activations[:-1]]
+MI = [MI_layerwise(data[:, -1], T) for T in activations]
+plt.figure()
+for layer in range(len(MI)):
+    plt.scatter(MI[layer][0], MI[layer][1], c=AnalyzeWeights.color_sequence[layer])
+
+plt.savefig("IB plot.eps")
+t2 = time()
 axes = []
 autocorr = [None]*nlayers
 frac_corr = 10
@@ -141,15 +182,28 @@ for layer in range(nlayers):
     if saveplots:
         f.savefig("autocorr_layer" + str(layer + 1) + ".jpg")
 
-#TODO: next step is to implement the mean-std graph
-# if saveplots:
-#     nsamples = int(niterations / nb_epoch)
-#     for layer in range(nlayers):
-#         for t in range(nb_epoch):
-#             meandw[self.iter] = tmp_w[t * nsamples:(t + 1) * nsamples]
-#             stddw[self.iter] =
+if saveplots:
+    nsamples = int(niterations / nb_epoch)
+    mean_dw = np.empty((nb_epoch-1, nlayers))
+    std_dw = np.empty((nb_epoch-1, nlayers))
+    mean_w = np.empty((nb_epoch-1, nlayers))
+    f, a = plt.subplots(2, 1, sharex=True)
+    for layer in range(nlayers):
+        tmp_w = np.reshape(analyzeweights.w[layer], (niterations, unitsperlayer[layer]*analyzeweights.nlinksperunit[layer]))
+        tmp_dw = np.diff(tmp_w, axis=0)
+        for t in range(nb_epoch-1):
+            mean_w[t, layer] = np.linalg.norm(np.mean(tmp_w[t * nsamples:(t + 1) * nsamples, :], axis=0))
+            mean_dw[t, layer] = np.linalg.norm(np.mean(tmp_dw[t * nsamples:(t + 1) * nsamples, :], axis=0))/\
+                                mean_w[t, layer]
+            std_dw[t, layer] = np.linalg.norm(np.std(tmp_dw[t * nsamples:(t + 1) * nsamples, :], axis=0))/\
+                               mean_w[t, layer]
+        a[0].loglog(mean_dw[:, layer], color=AnalyzeWeights.color_sequence[layer])
+        a[1].loglog(std_dw[:, layer], '--', color=AnalyzeWeights.color_sequence[layer])
+    f.savefig("mean-std_plot.jpg")
+
 
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
-print('Time spent:', time() - t0)
+print('Total time spent:', time() - t0, ' MI computation time: ', t2 - t1, 'ANN computation time: ', t1 - t0,
+      ' plotting time: ', time() - t2)
---
+++ rnn_selfmade.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+
+
+class RNNNumpy:
+
+    def __init__(self, word_dim, out_dim, batch_size = 100, hidden_dim=100, bptt_truncate=4):
+        # Assign instance variables
+        self.word_dim = word_dim
+        self.out_dim = out_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        self.batch_size = batch_size
+        # Randomly initialize the network parameters
+        self.U = np.random.uniform(-np.sqrt( 1. / word_dim), np.sqrt( 1. / word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-np.sqrt( 1. / hidden_dim), np.sqrt( 1. / hidden_dim), (out_dim, hidden_dim))
+        self.bV = np.zeros((out_dim, 1))
+        self.W = np.eye((hidden_dim, hidden_dim))
+        self.bW = np.zeros((hidden_dim, 1))
+
+    def forward_propagation(self, x):
+        # The total number of time steps
+        T = x.shape[0]
+        # During forward propagation we save all hidden states in s because need them later.
+        # We add one additional element for the initial hidden, which we set to 0
+        s = np.zeros((T + 1, self.hidden_dim, x.shape[2]))
+        # For each time step...
+        for t in np.arange(T):
+            # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+            s[t] = self.U.dot(x[t]) + self.W.dot(s[t-1]) + self.bW
+            s[t] = self.rectify(s[t])
+
+        o = self.V.dot(s[-2]) + self.bV
+        return [o, s]
+
+    def rectify(self, x):
+        return np.multiply(x, (x > 0))
+
+    def predict(self, x):
+        # Perform forward propagation and return index of the highest score
+        o = self.forward_propagation(x)[0]
+        return self.rectify(o)
+
+    def bptt(self, x, y):
+        T = x.shape[0]
+        p = x.shape[2]
+        # Perform forward propagation
+        o, s = self.forward_propagation(x)
+        # We accumulate the gradients in these variables
+        delta_o = np.multiply(y - o, np.sign(o, 0))
+        dLdV = np.dot(delta_o, s[-2].T)
+        dLdbV = np.sum(delta_o, axis=1, keepdims=True)
+        # Initial delta calculation: dL/dz
+        # Backpropagation through time (for at most self.bptt_truncate steps)
+        dLdU = np.zeros(self.U.shape)
+        dLdW = np.zeros(self.W.shape)
+        dLdbW = np.zeros(self.bW.shape)
+        delta_t = self.V.T.dot(delta_o)
+        for bptt_step in np.arange(T):
+            # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
+            # Add to gradients at each previous step
+            tmp = np.multiply(delta_t, np.sign(s[-bptt_step - 2]))
+            dLdW += np.dot(tmp, s[-bptt_step - 3].T)
+            dLdbW += np.sum(tmp, axis=1, keepdims=True)
+            dLdU += np.dot(tmp, x[-bptt_step - 1].T)
+            # Update delta for next step dL/dz at t-1
+            delta_t = self.W.T.dot(tmp)
+
+        return {'dLdU':dLdU/p, 'dLdV':dLdV/p, 'dLdbV':dLdbV/p, 'dLdW':dLdW/p, 'dLdbW':dLdbW/p}
+
+
+    def calculate_loss(self, o, y):
+        return 0.5 * np.sum((o - y)**2) / o.shape[1]
+
+    # def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
+    #     # Calculate the gradients using backpropagation. We want to checker if these are correct.
+    #     bptt_gradients = self.bptt(x, y)
+    #     # List of all parameters we want to check.
+    #     model_parameters = ['U', 'V', 'W']
+    #     # Gradient check for each parameter
+    #     for pidx, pname in enumerate(model_parameters):
+    #         # Get the actual parameter value from the mode, e.g. model.W
+    #         parameter = operator.attrgetter(pname)(self)
+    #         print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+    #         # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+    #         it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+    #         while not it.finished:
+    #             ix = it.multi_index
+    #             # Save the original value so we can reset it later
+    #             original_value = parameter[ix]
+    #             # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+    #             parameter[ix] = original_value + h
+    #             gradplus = self.calculate_total_loss([x],[y])
+    #             parameter[ix] = original_value - h
+    #             gradminus = self.calculate_total_loss([x],[y])
+    #             estimated_gradient = (gradplus - gradminus)/(2*h)
+    #             # Reset parameter to original value
+    #             parameter[ix] = original_value
+    #             # The gradient for this parameter calculated using backpropagation
+    #             backprop_gradient = bptt_gradients[pidx][ix]
+    #             # calculate The relative error: (|x - y|/(|x| + |y|))
+    #             relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+    #             # If the error is to large fail the gradient check
+    #             if relative_error > error_threshold:
+    #                 print("Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix))
+    #                 print("+h Loss: %f" % gradplus)
+    #                 print("-h Loss: %f" % gradminus)
+    #                 print("Estimated_gradient: %f" % estimated_gradient)
+    #                 print("Backpropagation gradient: %f" % backprop_gradient)
+    #                 print("Relative Error: %f" % relative_error)
+    #                 return
+    #             it.iternext()
+    #         print("Gradient check for parameter %s passed." % (pname))
+
+
+def gendata(T=7, num=1000):
+    x = np.random.rand((T, num))
+    # idx = np.random.choice(T, (2, num), replace=False)
+    y = np.zeros(T, num)
+    for i in range(num):
+        y[:, i] = np.random.randint(T, size=T)
+
+    return (x, y)
